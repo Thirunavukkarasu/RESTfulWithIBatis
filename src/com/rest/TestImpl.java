7e47c408b4e46010cd0298c55d30e59855b8ca41
Thirunavukkarasu Muthuswamy <Thirunavukkarasu.Muthuswamy.ap@gmail.com>
2014-12-17 01:48:44 +0530

    first commit

---
+++ src/com/rest/TestImpl.java
@@ -0,0 +1,30 @@
+ 
+package com.rest;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.pojo.Products;
+
+
+@Path("TestImpl")
+public class TestImpl {
+	
+	@GET
+	@Produces({"application/json"})
+	public  List<Products> resourceMethodGET() throws IOException, SQLException { 
+		 Reader rd = Resources.getResourceAsReader("SqlMapConfig.xml");
+		 SqlMapClient smc = SqlMapClientBuilder.buildSqlMapClient(rd);
+		 List<Products> products = (List<Products>)smc.queryForList("Products.getAll");
+		 return products;
+	}
+}
